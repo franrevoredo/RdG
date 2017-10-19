15cfdbd67bcdbdeef78955cff66a198debe1aa9e
Francisco Revoredo <francisco.r.89@gmail.com>
2017-10-19 10:53:52 -0300

    Update gorecpass.php

--- gorecpass.php
+++ gorecpass.php
@@ -47,7 +47,6 @@ if ((filter_input(INPUT_POST, 'user') == '') || (filter_input(INPUT_POST, 'mail'
 		$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;   // Enable SMTP authentication
 		$mail->Username = 'francisco.r.89@gmail.com';   // SMTP username
-		$mail->Password = 'bigpoppa';      // SMTP password
 		$mail->SMTPSecure = 'tls';       // Enable TLS encryption, `ssl` also accepted
 		$mail->Port = 587; // TCP port to connect to
 		$mail->CharSet = 'UTF-8';
